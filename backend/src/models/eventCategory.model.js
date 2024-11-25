22a1d27e7515acc6eb010862195a8a2faaf2bc1d
Shrishail20 <136682949+Shrishail20@users.noreply.github.com>
2024-11-25 17:31:48 +0530

    handled the error during the vendor and category creation during the description not pased

--- backend/src/models/eventCategory.model.js
+++ backend/src/models/eventCategory.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const eventCategorySchema = new mongoose.Schema({
     category_name: { type: String, required: true },  // Category name for the event
-    description: { type: String, required: true },  // Description of the category
+    description: { type: String },  // Description of the category
     image: { type: String },  // URL for the category image
     is_active: { type: String, default: '1' },  // '1' for active, '0' for inactive
     is_deleted : {type : String,default : '0'},
--- backend/src/models/vendor.model.js
+++ backend/src/models/vendor.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const vendorSchema = new mongoose.Schema({
     name: { type: String, required: true },  // Name of the vendor
-    description: { type: String, required: true },  // Description of the vendor
+    description: { type: String },  // Description of the vendor
     is_active: { type: String, default: '1' },  // '1' for active, '0' for inactive
     is_deleted : {type : String,default : '0'},
 }, {
